9a4942e2e142cb01af4bec11c99a7d54b5bee893
Panos Kountanis <panosktn@gmail.com>
2012-06-24 15:20:23 +0300

    refactoring tests to use a fixture

--- lib/stack_test.cc
+++ lib/stack_test.cc
@@ -7,30 +7,29 @@ void str_freefn(void *str)
 	free(*(char **) str);
 }
 
-TEST(Stack_push, PushesValueToStackIncreaseLength) {
+class StackTest : public testing::Test {
+protected:
+	virtual void SetUp() { Stack_init(&s, sizeof(int), NULL); }
+	virtual void TearDown() { Stack_delete(&s); }
 	Stack s;
-	Stack_init(&s, sizeof(int), NULL);
+};
+
+TEST_F(StackTest, PushesValueToStackIncreaseLength) {
 	int pushVal = 1;
 	Stack_push(&s, (void *) &pushVal);
 	EXPECT_EQ(1, s.length);
-	Stack_delete(&s);
 }
 
-TEST(Stack_push, PushesValueToStackIncreaseLengthAndReallocIfInsufficient) {
-	Stack s;
-	Stack_init(&s, sizeof(int), NULL);
+TEST_F(StackTest, PushesValueToStackIncreaseLengthAndReallocIfInsufficient) {
 	int pushVal = 1;
 	for (int i = 0; i < 5; i++) {
 		Stack_push(&s, (void *) &pushVal);
 	}
 
 	EXPECT_EQ(5, s.length);
-	Stack_delete(&s);
 }
 
-TEST(Stack_pop, PopFromTopOfStack) {
-	Stack s;
-	Stack_init(&s, sizeof(int), NULL);
+TEST_F(StackTest, PopFromTopOfStack) {
 	int pushVal[3] = {1, 2, 3};
 	int popVal;
 
@@ -41,28 +40,23 @@ TEST(Stack_pop, PopFromTopOfStack) {
 	Stack_pop(&s, &popVal);
 	EXPECT_EQ(2, s.length);
 	EXPECT_EQ(3, popVal);
-	Stack_delete(&s);
 }
 
-TEST(Stack_pop, NotWriteAnythingIfNoElemsInStack) {
-	Stack s;
-	Stack_init(&s, sizeof(int), NULL);
+TEST_F(StackTest, NotWriteAnythingIfNoElemsInStack) {
 	int popVal = NULL;
-
 	Stack_pop(&s, &popVal);
 	EXPECT_EQ(NULL, popVal);
-	Stack_delete(&s);
 }
 
-TEST(Stack_delete, DeleteOwnedElements) {
-	Stack s;
-	Stack_init(&s, sizeof(char *), str_freefn);
+TEST_F(StackTest, DeleteOwnedElements) {
+	Stack d;
+	Stack_init(&d, sizeof(char *), str_freefn);
 	const char *pushVals[3] = {"Alice", "Bob", "Carl"};
 
 	for (int i = 0; i < 3; i++) {
 		char *name = strdup(pushVals[i]);
-		Stack_push(&s, (void *) &name);
+		Stack_push(&d, (void *) &name);
 	}
 
-	Stack_delete(&s);
+	Stack_delete(&d);
 }
